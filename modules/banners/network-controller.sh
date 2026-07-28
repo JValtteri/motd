5e345a3f69921ce29f5f3487171548083fbe11ec
JValtteri <46611993+JValtteri@users.noreply.github.com>
2026-07-28 19:44:52 +0300

    Added more premade custom static banners

---
+++ modules/banners/network-controller.sh
@@ -0,0 +1,15 @@
+no_color=$(echo -e "\033[0m")
+yellow=$(echo -e "\033[1;33m")
+green=$(echo -e "\033[1;32m")
+red=$(echo -e "\033[1;31m")
+cyan=$(echo -e "\033[1;36m")
+
+# Fonts: https://www.asciiart.eu/text-to-ascii-art
+# Font: small
+echo "${cyan}"
+
+echo " _  _     _                  _      ___         _           _ _         "
+echo "| \| |___| |___ __ _____ _ _| |__  / __|___ _ _| |_ _ _ ___| | |___ _ _ "
+echo "| .\` / -_)  _\ V  V / _ \ '_| / / | (__/ _ \ ' \  _| '_/ _ \ | / -_) '_|"
+echo "|_|\_\___|\__|\_/\_/\___/_| |_\_\  \___\___/_||_\__|_| \___/_|_\___|_| "
+echo ""
---
+++ modules/banners/pi4server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+no_color=$(echo -e "\033[0m")
+yellow=$(echo -e "\033[1;33m")
+green=$(echo -e "\033[1;32m")
+red=$(echo -e "\033[1;31m")
+cyan=$(echo -e "\033[1;36m")
+
+# Fonts: https://www.asciiart.eu/text-to-ascii-art
+# Font: small
+echo "${yellow}"
+
+cat << EOF
+ ___ _ _ _    ___                      
+| _ (_) | |  / __| ___ _ ___ _____ _ _ 
+|  _/ |_  _| \__ \/ -_) '_\ V / -_) '_|
+|_| |_| |_|  |___/\___|_|  \_/\___|_|  
+
+EOF
