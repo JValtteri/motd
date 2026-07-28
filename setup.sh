d12fd1f97d49025ad94015f43e2d608246ab78c8
JValtteri <46611993+JValtteri@users.noreply.github.com>
2026-07-28 15:40:39 +0300

    Working updated setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-# Copy scripts to HOME
-cp -r ../motd ~/
+echo "Do you want to"
+echo "1) Install MOTD for Local user only (recommended)"
+echo "2) Install MOTD system wide (requires sudo)"
+echo "Q) Cancel"
+echo -e -n "\n>"
+read choise
 
-# Setup motd.sh to run at login
-echo "~/motd/motd.sh" >> ~/.bashrc
+if [ "$choise" == "1" ]; then
 
-echo "done"
+    echo "Installing motd.sh script for local user"
+
+    # Copy scripts to HOME
+    cp -r ../motd ~/
+
+    # Setup motd.sh to run at login
+    echo "~/motd/motd.sh" >> ~/.bashrc
+    echo "You can configure the script by editing ~/motd/motd.sh"
+
+elif [ "$choise" == "2" ]; then
+    echo "Installing motd.sh script system wide (needs sudo)"
+
+    # Setup motd.sh to run at login
+    echo "/etc/motd-script/motd.sh" >> /etc/profile
+    if [ "$?" == "0" ]; then
+        # Copy scripts to /etc/motd-script
+        cp -r ../motd /etc/motd-script/
+        echo "You can configure the script by editing /etc/motd-script/motd.sh"
+    else
+        echo "Please run the script with sudo"
+    fi
+else
+    echo "Cancelling"
+    exit
+fi
+
