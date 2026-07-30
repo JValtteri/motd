4f753a7715f9ffd963b6414022a85059fc6e6a4f
JValtteri <46611993+JValtteri@users.noreply.github.com>
2026-07-31 01:35:22 +0300

    Add comment to help identifying configuration lines created by motd

--- setup.sh
+++ setup.sh
@@ -16,36 +16,37 @@ MOTD_PATH=""
 
 if [ "$choise" == "1" ]; then
     echo "${yellow}Installing motd.sh script for local user${no_color}"
-
     MOTD_PATH=~/motd
+
+    # Setup motd.sh to run at login
+    echo "# MOTD script" >> ~/.bashrc
+    echo "export MOTD_PATH=${MOTD_PATH}" >> ~/.bashrc
+    echo "${MOTD_PATH}/motd.sh" >> ~/.bashrc
 
     # Copy scripts to HOME
     cp -r ../motd ~/
-
-    # Setup motd.sh to run at login
-    echo "export MOTD_PATH=${MOTD_PATH}" >> ~/.bashrc
-    echo "${MOTD_PATH}/motd.sh" >> ~/.bashrc
 
     echo "${yellow}You can configure the script by editing ${MOTD_PATH}/motd.sh${no_color}"
 
 
 elif [ "$choise" == "2" ]; then
     echo "${yellow}Installing motd.sh script system wide (needs sudo)${no_color}"
-
     MOTD_PATH=/etc/motd-script
 
     # Setup motd.sh to run at login
+    echo "# MOTD script" >> /etc/profile
     echo "export MOTD_PATH=${MOTD_PATH}" >> /etc/profile
     echo "$MOTD_PATH/motd.sh" >> /etc/profile
+
     if [ "$?" == "0" ]; then
         # Copy scripts to /etc/motd-script
         cp -r ../motd "${MOTD_PATH}/"
-
         echo "${yellow}You can configure the script by editing ${MOTD_PATH}/motd.sh${no_color}"
     else
         echo "${red}Please run the script with sudo${no_color}"
         exit
     fi
+
 else
     echo "Cancelled"
     exit
